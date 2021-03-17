9a090ff25ab841aa794b5e41751d24634c86938e
Kittitach Piyamapornchai <bankgg@gmail.com>
2021-03-17 13:58:56 +0700

    Fix

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,83 +1,43 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
+  <section class="section">
+    <div class="columns is-mobile">
+      <card title="Free" icon="github">
+        Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
+      </card>
 
-      <p>
-        Message: {{ message }}
-      </p>
+      <card title="Responsive" icon="cellphone-link">
+        <b class="has-text-grey"> Every </b> component is responsive
+      </card>
 
-      <h1 class="title">
-        client
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <card title="Modern" icon="alert-decagram">
+        Built with <a href="https://vuejs.org/"> Vue.js </a> and
+        <a href="http://bulma.io/"> Bulma </a>
+      </card>
+
+      <card title="Lightweight" icon="arrange-bring-to-front">
+        No other internal dependency
+      </card>
+
+      <card title="Eieieiei" icon="arrange-bring-to-front">
+        {{ message }}
+      </card>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
+import Card from '~/components/Card'
+
 export default {
+  name: 'HomePage',
+
+  components: {
+    Card,
+  },
+
   async asyncData({ $axios }) {
     const message = await $axios.$get('http://server:8080')
     return { message }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
--- server/go.mod
+++ server/go.mod
@@ -15,5 +15,5 @@ require (
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gorm.io/gorm v1.20.12 // indirect
+	gorm.io/driver/mysql v1.0.5 // indirect
 )
